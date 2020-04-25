691e8066042a8a864aca7179f1b1af5da85b43af
norvec <greentech676@gmail.com>
2020-04-26 01:22:41 +0400

    first commit

---
+++ Site/Feedback.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Cluster Main Page</title>
+
+	<script type="text/javascript" src="http://www.rudebox.org.ua/js/jquery.js"></script>
+	<!---        <link rel="shortcut icon" href="http://www.rudebox.org.ua/wp-content/themes/mystique/favicon.ico" /> --->
+	<link media="screen" href="demo/styles/demo.css" type="text/css" rel="stylesheet" />
+	<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="css/demo.css" />
+	<link rel="stylesheet" type="text/css" href="css/component.css" />
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Raleway:200,400,800' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+	<div class="container demo-1">
+		<div class="content">
+			<div id="large-header" class="large-header">
+				<div class="feedback">
+					<div><i class="material-icons">email</i>greentech676@gmail.com </div>
+					<div><i class="material-icons">near_me </i> t.me/nnorienn</div>
+				</div>
+			</div>
+
+			<script src="js/TweenLite.min.js"></script>
+			<script src="js/EasePack.min.js"></script>
+			<script src="js/rAF.js"></script>
+			<script src="js/demo-1.js"></script>
+</body>
+
+</html>
---
+++ Site/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Cluster Main Page</title>
+	<style>
+		@import url('https://fonts.googleapis.com/css?family=Raleway&display=swap');
+	</style>
+	<script type="text/javascript" src="http://www.rudebox.org.ua/js/jquery.js"></script>
+	<!---        <link rel="shortcut icon" href="http://www.rudebox.org.ua/wp-content/themes/mystique/favicon.ico" /> --->
+	<link media="screen" href="demo/styles/demo.css" type="text/css" rel="stylesheet" />
+	<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="css/demo.css" />
+	<link rel="stylesheet" type="text/css" href="css/component.css" />
+	<link href='http://fonts.googleapis.com/css?family=Raleway:200,400,800' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+	<div class="container demo-1">
+		<div class="content">
+			<div id="large-header" class="large-header">
+				<a class="main-title" href="http://cluster.sgu.ru/">Cluster<span class="thin"> SSU</span></a>
+				<nav class="codrops-demos">
+					<div class="midder">
+						<a class="current-demo1" href="http://cluster.sgu.ru:9098/">Очередь</a>
+						<a class="current-demo2" href="http://cluster.sgu.ru:3000/">Grafana</a>
+						<a class="current-demo3" href="http://cluster.sgu.ru/cluster_guide_new.pdf">Гайд</a>
+						<a class="current-demo4" href="./Forma.docx">Доступ</a>
+						<a class="current-demo5" href="./Feedback.php">Обратная связь</a>
+					</div>
+				</nav>
+				<canvas id="demo-canvas"></canvas>
+			</div>
+
+			<script src="js/TweenLite.min.js"></script>
+			<script src="js/EasePack.min.js"></script>
+			<script src="js/rAF.js"></script>
+			<script src="js/demo-1.js"></script>
+</body>
+
+</html>
